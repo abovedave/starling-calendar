2c49b413aa6d0e56c81434a911a2b9a9dd839d59
David Longworth <mr@longworth.to>
2021-05-26 19:08:33 +0100

    server setup

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ const formatIcsDate = (ts, nextDay) => {
 
 const start = async () => {
   try {
-    await fastify.listen(443)
+    await fastify.listen(process.env.PORT, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
